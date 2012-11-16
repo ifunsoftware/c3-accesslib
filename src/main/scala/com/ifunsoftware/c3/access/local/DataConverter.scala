e9afb728b6445b6cc35049d2de9c329607996e05
Mikhail Malygin <mikhail.malygin@gmail.com>
2012-11-17 02:20:07 +0400

    [Fixed] issues with domainId

--- src/main/scala/com/ifunsoftware/c3/access/local/DataConverter.scala
+++ src/main/scala/com/ifunsoftware/c3/access/local/DataConverter.scala
@@ -4,13 +4,6 @@ import com.ifunsoftware.c3.access.impl.{FileDataStream, ByteArrayDataStream}
 import org.aphreet.c3.platform.resource.{DataStream => InternalDataStream}
 import com.ifunsoftware.c3.access.DataStream
 
-/**
- * Created with IntelliJ IDEA.
- * User: malygm
- * Date: 11/14/12
- * Time: 7:57 PM
- * To change this template use File | Settings | File Templates.
- */
 trait DataConverter {
 
   implicit def streamToInternalStream(datastream:DataStream):InternalDataStream = {
--- src/main/scala/com/ifunsoftware/c3/access/local/LocalC3System.scala
+++ src/main/scala/com/ifunsoftware/c3/access/local/LocalC3System.scala
@@ -9,16 +9,29 @@ import org.aphreet.c3.platform.filesystem.FSManager
 import scala.Some
 import org.osgi.framework.BundleContext
 import org.slf4j.LoggerFactory
+import org.aphreet.c3.platform.domain.DomainManager
 
 class LocalC3System(val domain: String, val bundleContext:AnyRef) extends C3System with DataConverter{
 
-  var accessManager = resolveService(classOf[AccessManager])
+  val accessManager = resolveService(classOf[AccessManager])
 
-  var fsManager = resolveService(classOf[FSManager])
+  val fsManager = resolveService(classOf[FSManager])
 
-  var accessControlManager: AccessControlManager = resolveService(classOf[AccessControlManager])
+  val accessControlManager: AccessControlManager = resolveService(classOf[AccessControlManager])
 
-  val accessControlParams = Map("domain" -> domain)
+  val domainManager: DomainManager = resolveService(classOf[DomainManager])
+
+  val domainId = {
+    domainManager.domainById(domain) match {
+      case Some(domainInstance) => domainInstance.id
+      case None => domainManager.domainList.filter(_.name == domain).headOption match {
+        case Some(domainInstance) => domainInstance.id
+        case None => throw new C3AccessException("Can't find domain: " + domain)
+      }
+    }
+  }
+
+  val accessControlParams = Map("domain" -> domainId)
 
   def fetchResource(ra: String):Resource = {
     accessManager.getOption(ra) match {
@@ -61,7 +74,7 @@ class LocalC3System(val domain: String, val bundleContext:AnyRef) extends C3Syst
   }
 
   def getFile(name: String) = {
-    val internalNode = fsManager.getNode(domain, name)
+    val internalNode = fsManager.getNode(domainId, name)
 
     retrieveAccessTokens(READ).checkAccess(internalNode.resource)
 
@@ -82,11 +95,11 @@ class LocalC3System(val domain: String, val bundleContext:AnyRef) extends C3Syst
   }
 
   def deleteFile(name: String) {
-    val node = fsManager.getNode(domain, name)
+    val node = fsManager.getNode(domainId, name)
 
     retrieveAccessTokens(DELETE).checkAccess(node.resource)
 
-    fsManager.deleteNode(domain, name)
+    fsManager.deleteNode(domainId, name)
   }
 
   def search(query: String):List[SearchResultEntry] = null
@@ -103,7 +116,7 @@ class LocalC3System(val domain: String, val bundleContext:AnyRef) extends C3Syst
 
     retrieveAccessTokens(CREATE)
 
-    fsManager.createDirectory(domain, fullName)
+    fsManager.createDirectory(domainId, fullName)
   }
 
   def createFile(fullName:String, meta: Map[String, String], data: DataStream){
@@ -117,11 +130,11 @@ class LocalC3System(val domain: String, val bundleContext:AnyRef) extends C3Syst
 
     accessTokens.updateMetadata(resource)
 
-    fsManager.createFile(domain ,fullName, resource)
+    fsManager.createFile(domainId ,fullName, resource)
   }
 
   def move(oldPath: String, newPath: String){
-    fsManager.moveNode(domain, oldPath, newPath)
+    fsManager.moveNode(domainId, oldPath, newPath)
   }
 
   def resolveService[T](clazz:Class[T]):T = {
